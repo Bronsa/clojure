9bd1476dc43d59d910f23b07970bb55ac84126d4
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2012-11-15 19:33:47 -0800

    CLJ-99: Make min-key and max-key evaluate k on each argument at most once
    Previous versions of these functions evaluated k on most argument
    twice if there were 3 or more.

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -4598,7 +4598,15 @@
   ([k x] x)
   ([k x y] (if (> (k x) (k y)) x y))
   ([k x y & more]
-   (reduce1 #(max-key k %1 %2) (max-key k x y) more)))
+   (let [kx (k x) ky (k y)
+         [v kv] (if (> kx ky) [x kx] [y ky])]
+     (loop [v v kv kv more more]
+       (if-let [w (first more)]
+         (let [kw (k w)]
+           (if (> kw kv)
+             (recur w kw (next more))
+             (recur v kv (next more))))
+         v)))))
 
 (defn min-key
   "Returns the x for which (k x), a number, is least."
@@ -4607,7 +4615,15 @@
   ([k x] x)
   ([k x y] (if (< (k x) (k y)) x y))
   ([k x y & more]
-   (reduce1 #(min-key k %1 %2) (min-key k x y) more)))
+   (let [kx (k x) ky (k y)
+         [v kv] (if (< kx ky) [x kx] [y ky])]
+     (loop [v v kv kv more more]
+       (if-let [w (first more)]
+         (let [kw (k w)]
+           (if (< kw kv)
+             (recur w kw (next more))
+             (recur v kv (next more))))
+         v)))))
 
 (defn distinct
   "Returns a lazy sequence of the elements of coll with duplicates removed"
