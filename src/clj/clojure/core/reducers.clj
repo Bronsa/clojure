9b84f92d4a3bf1a9b8763a3dbe7dbdc8816624ca
Paul Butcher <paul@paulbutcher.com>
2013-04-10 16:20:36 +0100

    Add foldable-seq

--- src/clj/clojure/core/reducers.clj
+++ src/clj/clojure/core/reducers.clj
@@ -360,3 +360,29 @@
  (coll-fold
   [m n combinef reducef]
   (.fold m n combinef reducef fjinvoke fjtask fjfork fjjoin)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;; foldable lazy sequences ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- foldseq [num-tasks coll chunk-size combinef reducef]
+  (fjinvoke (fn []
+    (let [chunk-task (fn [chunk] (fjfork (fjtask #(reduce reducef (combinef) chunk))))]
+      (reduce combinef
+        (core/map #(fjjoin (first (doall %)))
+          (partition-all num-tasks 1 (core/map chunk-task (partition-all chunk-size coll)))))))))
+
+(defn foldable-seq
+  "Given a sequence, return a sequence that supports parallel fold.
+  The sequence is consumed incrementally in chunks of size n
+  (where n is the group size parameter passed to fold). No more than
+  num-tasks (default 10) will be processed in parallel."
+  ([coll] (foldable-seq 10 coll))
+  ([num-tasks coll]
+    (reify
+      clojure.core.protocols/CollReduce
+      (coll-reduce [_ f]
+        (clojure.core.protocols/coll-reduce coll f (f)))
+      (coll-reduce [_ f init]
+        (clojure.core.protocols/coll-reduce coll f init))
+
+      CollFold
+      (coll-fold [_ n combinef reducef]
+        (foldseq num-tasks coll n combinef reducef)))))
